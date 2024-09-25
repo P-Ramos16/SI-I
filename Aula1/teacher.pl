d0a5faf3646b86f453a1c63b464efdc7772734b1
P-Ramos16 <p.ramos@ua.pt>
2024-09-25 11:07:18 +0100

    Added some more examples for recursive functions

--- Aula1/teacher.pl
+++ Aula1/teacher.pl
@@ -2,9 +2,28 @@ teaches(lsl, si).
 woman(joana).
 student(alfredo, mei, ua).
 
-teacher(X) :-teaches(X, _)
+teacher(X) :- teaches(X, _).
 
 man(socrates).
 mortal(X) :- man(X).
 
-uncle(X, Y) :- brother(X, Z), parent(Z Y).
+uncle(X, Y) :- brother(X, Z), parent(Z, Y).
+
+occurs(X, [X|_]).
+
+occurs(X, [_ | T]) :- 
+  occurs(X, T).
+
+
+len([], 0).
+
+len([_|L], N) :-
+  len(L, Nt), 
+  N is 1 + Nt.
+
+
+concatenate(L1, [], L1).
+concatenate([], L2, L2).
+
+concatenate([H1 | T1], L2, [H1|Lt]) :-
+    concatenate(T1, L2, Lt).
